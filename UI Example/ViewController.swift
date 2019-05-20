6132fead4ce0f80156cbe522d64c5af50290191f
Maxime <maxime.alfosea@hotmail.fr>
2019-05-20 16:21:51 +0200

    Essai sur les View dans Xcode

--- UI Example/ViewController.swift
+++ UI Example/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var ui_title_label: UILabel!
+    @IBOutlet weak var ui_editName: UITextField!
+    @IBOutlet weak var ui_age_label: UILabel!
+    @IBOutlet weak var ui_story_edit: UITextField!
+    @IBOutlet weak var ui_story_text_area: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btn_submitButton() {
+        if let name = ui_editName.text {
+            if name == "" {
+            ui_title_label.text = "Pas de nom ?"
+            } else {
+                ui_title_label.text = name
+            }
+        } else {
+            ui_title_label.text = "Personne"
+        }
+    }
+    @IBAction func slider_age(_ sender: UISlider) {
+        let ageTemp = Int(sender.value)
+        ui_age_label.text = "Tu as \(ageTemp) ans"
+    }
+    @IBAction func btn_story_submit(_ sender: Any) {
+        if let text = ui_story_edit.text {
+            if text == "" {
+                
+            } else {
+            ui_story_text_area.text += text + "\n"
+            ui_story_edit.text = ""
+            }
+        }
+    }
 }
 
